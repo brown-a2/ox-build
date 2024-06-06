b140c566df105351756d27fc6f7ecbdbad56eaec
adam <brown-a2@users.noreply.github.com>
2024-06-06 16:03:17 +0100

    Update

--- bin/install_repo.sh
+++ bin/install_repo.sh
@@ -8,7 +8,7 @@ echo "Current directory: $(pwd)"
 # Set the GitHub repository URL, target directory, and Docker Compose file
 REPO_URL="https://github.com/brown-a2/ox-build.git"
 TARGET_DIR="/usr/local/bin/ox-build"
-DOCKER_COMPOSE_FILE="docker-compose-prod.yml"
+#DOCKER_COMPOSE_FILE="docker-compose-prod.yml"
 
 # Clone the repository
 git clone "$REPO_URL" "$TARGET_DIR"
