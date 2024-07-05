66132efe4ab3ea66864c11605ec7194c22e55e03
adam <brown-a2@users.noreply.github.com>
2024-07-05 16:47:49 +0100

    Fix local dev bug s3 uploads

--- bin/install_repo.sh
+++ bin/install_repo.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Install script. Copy into /usr/local/bin before setting up server.
 set -e
 
 # Debug output
@@ -9,10 +8,8 @@ echo "Current directory: $(pwd)"
 # Set the GitHub repository URL, target directory, and Docker Compose file
 REPO_URL="https://github.com/brown-a2/ox-build.git"
 TARGET_DIR="/usr/local/bin/ox-build"
-#DOCKER_COMPOSE_FILE="docker-compose-prod.yml"
 
 # Clone the repository
 git clone "$REPO_URL" "$TARGET_DIR"
 
-# Navigate to the target directory
-cd "$TARGET_DIR"
+chown -R pi:pi ox-build
